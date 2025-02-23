3ab0fa6c09b21648808156269fc1d69d0407c0a5
Dino Charalambous <dinochara@gmail.com>
2025-02-23 14:46:32 +0200

    only reports on weather, not marine info

--- weatherapi.py
+++ weatherapi.py
@@ -1,11 +1,15 @@
 import requests
 import json
+from os import getenv
+from dotenv import load_dotenv
 
-#asklocation = input("Type weather location: ")
-asklocation = "muizenberg"
+load_dotenv()
+
+myapi_key = getenv('api_key')
+asklocation = input("Type weather location: ")
 dow = {1:'Sunday',2:'Monday',3:'Tuesday',4:'Wednesday',5:'Thursday',6:'Friday',7:'Saturday'}
 
-url = f"http://api.weatherapi.com/v1/current.json?key=03e08f13e544491fbb9114121252302&q={asklocation}&aqi=no"
+url = f"http://api.weatherapi.com/v1/current.json?key={myapi_key}&q={asklocation}&aqi=no"
 
 payload = {}
 headers = {}
@@ -13,7 +17,7 @@ headers = {}
 response = requests.request("GET", url, headers=headers, data=payload)
 result = response.json()
 
-#print(json.dumps(result,indent=4))
+print(json.dumps(result,indent=4))
 
 location = result['location']['name']
 region = result['location']['region']
@@ -22,5 +26,6 @@ day = result['current']['is_day']
 conditions = result['current']['condition']['text']
 wind_speed = result['current']['wind_kph']
 wind_direction = result['current']['wind_dir']
-wind_chill = result['']
-print(wind_direction)
+wind_chill = result['current']['windchill_c']
+wind_gusts = result['current']['gust_kph']
+print(wind_gusts)
