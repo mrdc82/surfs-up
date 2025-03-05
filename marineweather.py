35b57337de61221e5751bd8dca7cbdd7ff651ff4
Dino Charalambous <dinochara@gmail.com>
2025-03-05 16:45:57 +0200

    calling compass into main module

--- marineweather.py
+++ marineweather.py
@@ -8,6 +8,7 @@ import condition_locations
 import location_data
 from pprint import pprint
 import numpy as np
+import compass
 
 
 # Some print stuff for cleanliness
@@ -89,7 +90,7 @@ for l in gl.locations:
     loc_name = l.capitalize().replace('_', ' ')
     get_weather()
     get_marine_data()
-    location_data.add_entry(loc_name, current_wind_wave_direction, weather_current_wind_speed_10m, current_swell_wave_direction, current_swell_wave_height,
+    location_data.add_entry(loc_name, compass.curr_wind_direction, weather_current_wind_speed_10m, compass.curr_swell_direction, current_swell_wave_height,
                             current_swell_wave_period, current_sea_surface_temperature)
 
 # Convert list to NumPy array
