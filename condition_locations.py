d8d8b0203c52c5d955ee3abca0e76881bb858859
Dino Charalambous <dcharalambous@fnb.co.za>
2025-03-17 15:21:09 +0200

    updated geolocations according to surfline

--- condition_locations.py
+++ condition_locations.py
@@ -14,25 +14,14 @@
 # ---------------------------------------------------------------------------------------------------------
 # ---------------------------------------------------------------------------------------------------------
 
-# Lists with descriptions
-
-south_desc = ['Llandudno (beautiful beach, heavy waves)','Dungeons (Hout Bay) (big wave spot)','Die Kom (great left-hand point break)','Misty Cliffs (fun for barrels)','Scarborough (powerful beach break)',
-            'Long beach (Kommetjie) (best for all skill levels, clean waves)']
-north_desc = ['Muizenberg (best for beginners, long rides)','Kalk Bay Reef (for experienced surfers, heavy barrel)','Fish Hoek (good for longboarders)','Dangers beach (hollow waves, intermediate to advanced)',
-            'Strand (beach break, works well in north winds)']
-east_desc  = ['Big Bay (messy but can be okay)','Blouberg (kite surfers love this wind, but surf can be bumpy)','Llandudno (protected enough to handle some east winds)',]
-west_desc  = ['Muizenberg (offshore and clean)','Kalk Bay Reef (epic barrels)','Strand (long rides, fun waves)','Dangers beach (sharper peaks, hollower waves)']
-
-# Lists without descriptions used for ingesting into location search
-
-south_west = ['Black rocks', 'Dungeons', 'Long beach', 'Port elizabeth']
+south_west = ['Black rocks', 'Dungeons', 'Long beach']
 north_east = ['Big bay', 'Derde steen', 'Horse trails', 'The wreck', 'Kamers']
-north_west = ['Muizenberg', 'Kalk bay reef', 'Misty cliffs', 'Barclays', 'Jeffreys bay']
+north_west = ['Muizenberg', 'Kalk bay reef', 'Misty cliffs', 'Barclays']
 south_east = ['Clifton first beach', 'Moses', 'Dunes', 'Die kom', 'Ledge', 'Kessels', 'Llandudno', 
              'Milnerton lighthouse', 'Misty cliffs', 'Sunset', 'Scarborough', 'Sandy bay', 'Solly\'s', 'The hoek', 
              'The wedge', 'Thermopylae']
 
-south = ['Llandudno','Dungeons','Die kom','Misty cliffs','Scarborough','Long beach', 'Port elizabeth']
+south = ['Llandudno','Dungeons','Die kom','Misty cliffs','Scarborough','Long beach']
 north = ['Muizenberg','Kalk bay reef','Fish hoek','Dangers beach','Strand']
 east  = ['Big bay','Blouberg','Llandudno']
-west  = ['Muizenberg','Kalk bay reef','Strand','Dangers beach', 'Black rocks', 'Jeffreys bay']
+west  = ['Muizenberg','Kalk bay reef','Strand','Dangers beach', 'Black rocks']
